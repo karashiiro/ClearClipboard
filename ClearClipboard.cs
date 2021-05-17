ad9e383524e378061310611954a42b1740e115d7
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-05-17 13:18:39 -0700

    Add project files.

---
+++ ClearClipboard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClearClipboard
+{
+    public static class ClearClipboard
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Clipboard.Clear();
+        }
+    }
+}
